3653ca39103048e8ec07eccfc58fb8c4f18fdc43
bluVshal <blu.vishal@gmail.com>
2023-02-07 00:56:21 +0400

    reworks

--- sql/sql.js
+++ sql/sql.js
@@ -3,7 +3,7 @@ const mysql_connector = require('mysql');
 const connection = mysql_connector.createConnection({
     host : 'localhost',
     user : 'root',
-    password  :'jq88esfx',
+    password  :'************',
     database : 'shoppersparadise'
   });
 
